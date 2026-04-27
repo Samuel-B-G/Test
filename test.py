754d9b85b09ccc4385617872b8e3a798be1df16f
Samuel-B-G <samuel.bergstengrip@gmail.com>
2026-04-27 21:02:49 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+import requests
+import json
+
+file = open("key.txt", "r")
+KEY = file.read()
+file.close()
+
+file = open("device-id.txt", "r")
+DEVICE_ID = file.read()
+file.close()
+
+RTDB = "https://group11-plant4all-default-rtdb.europe-west1.firebasedatabase.app/"
+
+def saveToFirebase(temperature, humidity, light):
+	data = {"temperature": temperature,
+	"humidity": humidity,
+	"light": light}
+	response = requests.put(RTDB+"/deviceData/"+DEVICE_ID+".json?auth="+KEY,json=data)
+	print(response.json())
+
+saveToFirebase(21.5, 31, 216)
